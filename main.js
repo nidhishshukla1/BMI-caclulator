278c33546eeb5640866e5c0f24e72f76b4f98b78
Nidhish kumar Shukla <nidhishshukla1@gmail.com>
2023-09-22 18:53:24 +0530

    Version 2: Fixed Exception Handling, Bit of CSS

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    const height = document.getElementById('height').value/100.0;
-    const weight = document.getElementById('weight').value;
-    const bmi = weight/(height*height);
-    // console.log(bmi)
-    const textNode = document.createTextNode(`Your BMI is ${bmi}`);
-    // console.log(textNode)
-    document.getElementById('results').appendChild(textNode)
+    const height = parseFloat(document.getElementById('height').value)/100.0;
+    const weight = parseFloat(document.getElementById('weight').value);
+    const results = document.getElementById('results');
+    if(height == '' || height <0 || isNaN(height)){
+        results.innerHTML = "Please enter a valid height";
+        results.style.color = 'orange';
+    }
+    else if(weight == '' || weight <0 || isNaN(weight)){
+        results.innerHTML = "Please enter a valid weight";
+        results.style.color = 'orange';
+    }
+    else{
+        const bmi = (weight/(height*height)).toFixed(2);
+        results.innerHTML = `Your BMI is ${bmi}`;
+        results.style.color = 'green';
+    }
 })
